1a6f9156bae8d64edfb25ba529f956cde17f14e3
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-05-13 14:51:52 +0900

    Update app.js

--- advanced-integration/public/app.js
+++ advanced-integration/public/app.js
@@ -146,7 +146,7 @@ function getGoogleTransactionInfo(countryCode) {
       }
     ],
     countryCode: countryCode,
-    currencyCode: "NGN",
+    currencyCode: "TND",
     totalPriceStatus: "FINAL",
     totalPrice: "10",
     totalPriceLabel: "Total"
