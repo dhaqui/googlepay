a4e5c0e31a7a33f826f00e0f9eac78da55a7c2e4
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-05-13 14:51:09 +0900

    Update paypal-api.js

--- advanced-integration/paypal-api.js
+++ advanced-integration/paypal-api.js
@@ -22,7 +22,7 @@ export async function createOrder() {
       purchase_units: [
         {
           amount: {
-            currency_code: "NGN",
+            currency_code: "TND",
             value: purchaseAmount,
           },
           payee: {
