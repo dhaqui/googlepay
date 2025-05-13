bfe1e34e40f2d07c32b045217931c83d812a0d59
dhaqui <49834807+dhaqui@users.noreply.github.com>
2025-05-13 14:28:32 +0900

    Update app.js

--- advanced-integration/public/app.js
+++ advanced-integration/public/app.js
@@ -146,7 +146,7 @@ function getGoogleTransactionInfo(countryCode) {
       }
     ],
     countryCode: countryCode,
-    currencyCode: "RWF",
+    currencyCode: "GHS",
     totalPriceStatus: "FINAL",
     totalPrice: "10",
     totalPriceLabel: "Total"
